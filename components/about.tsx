832c4c1d400ed3b71560e10008a7bf1ff421b2b4
Soojungkim <soojung.kimm@gmail.com>
2024-06-13 12:12:21 -0700

    updated

--- components/about.tsx
+++ components/about.tsx
@@ -1,47 +1,46 @@
 "use client";
 
-import SectionHeading from './section-heading'
-import {motion} from 'framer-motion'
-import { useSectionInView } from '@/lib/hooks';
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function About() {
-
   const { ref } = useSectionInView("About");
 
   return (
-    <motion.section className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28" 
-    ref={ref}
-    initial={{ opacity: 0, y: 100 }}
-    animate={{ opacity: 1, y: 0 }}
-    transition={{delay: 0.175}}
-    id="about"
+    <motion.section
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      ref={ref}
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
     >
-        <SectionHeading> About me </SectionHeading>
-        <p className="mb-3">
+      <SectionHeading> About me </SectionHeading>
+      <p className="mb-3">
         After graduating with a degree in{" "}
-        <span className="font-medium">Zoology and Media Studies</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
+        <span className="font-medium">Zoology and Media Studies</span> and
+        adventuring in the field of{" "}
+        <span className="font-medium">event management</span>, I decided to
+        pursue my passion for programming. I enrolled in a coding bootcamp and
+        learned <span className="font-medium">full-stack web development</span>.{" "}
         <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect and continuation of learning experience. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
-        <span className="font-medium">
-          React, JS, Python, SQL, and Flask
-        </span>
-        . I am also familiar with TypeScript and Next.js. I am always looking to
+        problem-solving aspect and continuation of learning experience. I{" "}
+        <span className="underline">love</span> the feeling of finally figuring
+        out a solution to a problem. My core stack is{" "}
+        <span className="font-medium">React, JS, Python, SQL, and Flask</span>.
+        I am also familiar with TypeScript and Next.js. I am always looking to
         learn new technologies. I am currently looking for a{" "}
         <span className="font-medium">full-time position</span> as a software
         developer.
       </p>
 
       <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games and photography. I also enjoy{" "}
+        <span>When I'm not coding</span>, I enjoy playing video games and
+        photography. I also enjoy{" "}
         <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">TypeScript and Algo</span>.
+        learning about <span className="font-medium">TypeScript and Algo</span>.
       </p>
     </motion.section>
-  )
+  );
 }
--- components/intro.tsx
+++ components/intro.tsx
@@ -74,7 +74,7 @@ export default function Intro() {
           Contact me here <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition"/>
         </Link>
 
-        <a className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10" href="/SJK_Resume.pdf" download>
+        <a className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10" href="/SJK_Technical_Resume.pdf" download>
           Download Resume <HiDownload className="opacity-60 group-hover:translate-y-1 transition"/>
         </a>
 
